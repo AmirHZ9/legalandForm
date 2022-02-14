c73227b48bfd80adc932c9ed5cf1a465a8e3b275
unknown <amirzanganeh1999@gmail.com>
2022-02-14 23:42:56 +0330

    barkhatForm

--- Contract/Barkhat/js/Barkhat.js
+++ Contract/Barkhat/js/Barkhat.js
@@ -8,13 +8,12 @@ $(document).on("click", ".submit", function (event) {
   } else {
     $(this).parents("div").children("ol").append(`
         <li class="w-100 position-relative">
-        <p>${text}</p>  
+        <p class="edit">${text}</p>  
         <button class="btn-deleteTask" type="button">
         <svg class='closetbn-svg' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
                     <path d="M1.293 1.293a1 1 0 0 1 1.414 0L8 6.586l5.293-5.293a1 1 0 1 1 1.414 1.414L9.414 8l5.293 5.293a1 1 0 0 1-1.414 1.414L8 9.414l-5.293 5.293a1 1 0 0 1-1.414-1.414L6.586 8 1.293 2.707a1 1 0 0 1 0-1.414z"></path>
                   </svg>
         </button>
-        <button class="editbtn" type="button">ویرایش</button>
         <button class="updatebtn" type="button">تایید</button>
         
         <br>
@@ -27,28 +26,31 @@ $(document).on("click", ".submit", function (event) {
 });
 
 //ویرایش بند
-$(document).on("click", ".editbtn", function () {
-  var taskText = $.trim($(this).siblings("p").text());
 
+var editable  = false
+$(document).on("click", ".edit", function () {
+  var taskText = $(this).text();
   $(this).siblings(".updatebtn").css({ display: "inline-block" });
-  $(this).hide();
+
+if(editable == false){
 
   $(this)
-    .siblings("p")
     .html(
       " <span class='textarea' role='textbox'contenteditable>" +
         taskText +
         "</span>"
     );
+      editable = true
+}
 });
 
 // تایید ویرایش بند
 $(document).on("click", ".updatebtn", function () {
   var inputval = $(this).siblings("p").children("span").text();
-
+editable = false
   $(this).siblings("p").text(inputval);
 
-  $(".editbtn").show();
+  
   $(".updatebtn").css({ display: "none" });
 });
 
@@ -63,13 +65,16 @@ $(document).on("click", ".btn-deleteTask", function (event) {
 
 //اضافه کردن ماده
 var numberofrules = 6;
+var lastRulse =  Number($('.lastRules').text())
 $(".addrules").click(function () {
   numberofrules++;
-
+  lastRulse++
+  $('.tedadeMade').text(lastRulse)
+  $('.lastRules').text(lastRulse)
   $(`
         <div class="border m-3 p-3 extraRules"> 
         <div class="d-flex ">
-        <p class="ms-3">ماده ${numberofrules}:</p>
+        <p class="ms-3">ماده <span class="number">${numberofrules}</span> :</p>
         <div class="d-flex justify-content-around position-relative">
         <p class="w-50 ">
         <span class="textarea w-100" role="textbox" data-placeholder="نوشتن..." contenteditable></span>
@@ -78,7 +83,6 @@ $(".addrules").click(function () {
         <button class="updateRulesTitle" type="button">تایید</button>
         </div>
         </div>  
-        
         <ol class="p-3">
         
         </ol>
@@ -90,7 +94,7 @@ $(".addrules").click(function () {
         <button class="deleterules" type="button">حذف</button>
         </div>
         </div>
-        `).insertBefore($(".addrules").parents("div"));
+        `).insertBefore($(".newRules"));
 });
 
 // ویرایش  تیتر ماده
@@ -111,7 +115,8 @@ $(document).on("click", ".editRulesTitle", function () {
 
 // تایید ویرایش تیتر ماده
 $(document).on("click", ".updateRulesTitle", function () {
-  var inputval = $(this).siblings("p").children("span").text();
+ 
+ var inputval = $(this).siblings("p").children("span").text();
 
   $(this).siblings("p").text(inputval);
 
@@ -121,8 +126,21 @@ $(document).on("click", ".updateRulesTitle", function () {
 
 // حذف ماده
 $(document).on("click", ".deleterules", function () {
+  numberofrules--
+  lastRulse--
+  $('.tedadeMade').text(lastRulse)
+  $('.lastRules').text(lastRulse)
+
+  const num =$(this).parents('div').nextAll(".extraRules").children("div:first").children("p").children('span').text()-1
+
+  $(this).parents('div').nextAll(".extraRules").children("div:first").children("p").children('span').text(num)
+
+
   $(this).parents(".extraRules").remove();
 });
+
+
+
 
 // form Validate/
 (function () {
