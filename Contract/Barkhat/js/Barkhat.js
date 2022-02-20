4235140cfd8273bf56cd288d728ebad830cb6f71
unknown <amirzanganeh1999@gmail.com>
2022-02-20 18:46:55 +0330

    final functionality

--- Contract/Barkhat/js/Barkhat.js
+++ Contract/Barkhat/js/Barkhat.js
@@ -1,36 +1,82 @@
-//اضاقه کردن بند به ماده
-$(document).on("click", ".submit", function (event) {
-  event.preventDefault();
+//ویرایش تبصره
+var numberOfTotalTabsare = 0
+var editable  = false
+$(document).on("click",".editTabsare",function(){
+  var taskText = $(this).text();
+  $(this).siblings('.addTabsare').css({display:"block"})
+  $(this).addClass('selected');
 
-  var text = $(this).siblings("p").children(".textarea").text();
-
-  if (text.length == 0) {
-  } else {
-    $(this).parents("div").children("ol").append(`
-        <li class="w-100 position-relative">
-        <p class="edit">${text}</p>  
-        <button class="btn-deleteTask" type="button">
-        <svg class='closetbn-svg' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
-                    <path d="M1.293 1.293a1 1 0 0 1 1.414 0L8 6.586l5.293-5.293a1 1 0 1 1 1.414 1.414L9.414 8l5.293 5.293a1 1 0 0 1-1.414 1.414L8 9.414l-5.293 5.293a1 1 0 0 1-1.414-1.414L6.586 8 1.293 2.707a1 1 0 0 1 0-1.414z"></path>
-                  </svg>
-        </button>
-        <button class="updatebtn" type="button">تایید</button>
-        
-        <br>
-        
-        </li>
-        `);
+  if(editable == false){
+    $(this)
+    .html(
+      " <span class='textarea' role='textbox'contenteditable>" +
+      taskText +
+      "</span>"
+      );
+      $(`.editTabsare:not('.selected')`).addClass('disabledbutton')
+        editable = true
+       
   }
 
-  $(".textarea").text("");
-});
+})
 
+//اضافه کردن تبصره
+$(document).on("click",".addTabsare",function(){
+  
+  number = Number($(this).siblings('span').children('.numberofTabsare').text())
+  var inputval = $(this).siblings("p").children("span").text();
+  $(this).siblings("p").text(inputval);
+  $(this).css({display:"none"})
+  if($(this).parents('li').next().is('li') ||inputval.length == 0){
+  }else{
+    numberOfTotalTabsare++
+    $('.numberOfTotalTabsare').text(numberOfTotalTabsare)
+    $(this).siblings(".deleteTabsare").removeClass('disabledbutton')
+    $(this).siblings('.hideTabsare').css({opacity:"1"})
+    
+     $(this).parents('li').after(`
+     
+       <li class=" position-relative">
+        <span class="hideTabsare">تبصره <span class='numberofTabsare'>${number + 1}</span> : </span>     
+        <p class="editTabsare  d-inline-block w-75" data-placeholder="تبصره"></p>  
+         <button class="deleteTabsare disabledbutton" type="button">
+           <svg class='closetbn-svg' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+             <path d="M1.293 1.293a1 1 0 0 1 1.414 0L8 6.586l5.293-5.293a1 1 0 1 1 1.414 1.414L9.414 8l5.293 5.293a1 1 0 0 1-1.414 1.414L8 9.414l-5.293 5.293a1 1 0 0 1-1.414-1.414L6.586 8 1.293 2.707a1 1 0 0 1 0-1.414z"></path>
+           </svg>
+         </button>
+         <button class="addTabsare" type="button">تایید</button>
+         <br>
+         </li>
+  
+     `)
+   }
+   
+   $('.editTabsare:not(this)').removeClass('disabledbutton')
+   $(this).siblings("p").removeClass('selected')
+      editable = false
+
+  })
+
+//حذف تبصره
+$(document).on("click",".deleteTabsare",function(){
+  if($(this).siblings('p').text().length == 0){
+
+  }else{
+    numberOfTotalTabsare--
+    $('.numberOfTotalTabsare').text(numberOfTotalTabsare)
+  }
+  console.log(numberOfTotalTabsare)
+  $(this).parents('li').nextAll('li').children('span').children('.numberofTabsare').each(function(){
+    $(this).text(($(this).text())-1)
+  })
+  $(this).parents("li").remove()
+})
 
 //ویرایش بند
-var editable  = false
 $(document).on("click", ".edit", function () {
   var taskText = $(this).text();
-  $(this).siblings(".updatebtn").css({ display: "inline-block" });
+  $(this).siblings(".updatebtn").css({ display: "inline-block" })
+  $(this).parents('h1').siblings(".updatebtn").css({ display: "inline-block" });
   $(this).addClass('selected');
 
 if(editable == false){
@@ -41,67 +87,109 @@ if(editable == false){
     "</span>"
     );
     $(`.edit:not('.selected')`).addClass('disabledbutton')
+    
       editable = true
      
 }
 });
 
-// تایید ویرایش بند
+// تایید ویرایش بند و اضافه کردن بند به ماده
 $(document).on("click", ".updatebtn", function () {
   var inputval = $(this).siblings("p").children("span").text();
-  console.log(inputval.length)
+  var spanval = $(this).siblings('h1').children("span").text();
+
   editable = false
 
+    if($(this).parents('li').next().is('li') ||inputval.length == 0){
+
+    }else{
+
+      $(this).parents('li').after(`
+      <li class="w-100 position-relative">
+      <p class="edit mb-0" data-placeholder=". . . ."></p>   
+      <button class="btn-deleteTask" type="button">
+      <svg class='closetbn-svg' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                  <path d="M1.293 1.293a1 1 0 0 1 1.414 0L8 6.586l5.293-5.293a1 1 0 1 1 1.414 1.414L9.414 8l5.293 5.293a1 1 0 0 1-1.414 1.414L8 9.414l-5.293 5.293a1 1 0 0 1-1.414-1.414L6.586 8 1.293 2.707a1 1 0 0 1 0-1.414z"></path>
+                </svg>
+      </button>
+      <button class="updatebtn" type="button">تایید</button>
+      
+      <br>
+      
+      </li>
+      `)
+    }
 
   $('.edit:not(this)').removeClass('disabledbutton')
   $(this).siblings("p").removeClass('selected')
   $(this).siblings("p").text(inputval);
+  $(this).siblings("h1").children('span').text(spanval);
 
 
-  
   $(".updatebtn").css({ display: "none" });
 });
 
 // حذف کردن بند
-$(document).on("click", ".btn-deleteTask", function (event) {
-  event.preventDefault();
+$(document).on("click", ".btn-deleteTask", function () {
+  numberofTabsare--
+
+  $(this).parents('li').nextAll('li').children('p').children('span').children('.numberOfTabsare').each(function(){
+   $(this).text(($(this).text())-1)
+  })
+
 
   $(this).parents("li").siblings("input").remove();
   $(this).parents("li").siblings("label").remove();
   $(this).parents("li").remove();
+  
+  
+//
 });
 
 //اضافه کردن ماده
 var numberofrules = 6;
-var lastRulse =  Number($('.lastRules').text())
+var lastRules =  Number($('.lastRules').text())
 $(".addrules").click(function () {
   numberofrules++;
-  lastRulse++
+  lastRules++
   
-  $('.tedadeMade').text(lastRulse)
-  $('.lastRules').text(lastRulse)
+  $('.tedadeMade').text(lastRules)
+  $('.lastRules').text(lastRules)
   $(`
         <div class="m-3 p-3 extraRules"> 
-        <div class="d-flex made" >
-        <p class="ms-3">ماده <span class="number">${numberofrules}</span> :</p>
+        <div class="d-flex extraMade position-relative align-items-baseline" >
+        <p class="ms-3" >ماده <span class="number">${numberofrules}</span> :</p>
+        <button class="deleterules" type="button">
+          <svg class='closetbn-svg' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+            <path d="M1.293 1.293a1 1 0 0 1 1.414 0L8 6.586l5.293-5.293a1 1 0 1 1 1.414 1.414L9.414 8l5.293 5.293a1 1 0 0 1-1.414 1.414L8 9.414l-5.293 5.293a1 1 0 0 1-1.414-1.414L6.586 8 1.293 2.707a1 1 0 0 1 0-1.414z"></path>
+          </svg>
+        </button>
         <div class="d-flex position-relative w-75">
-        <p class=" editRulesTitle">
-        . . . .
-        </p>
+        <p class="editRulesTitle" data-placeholder=". . . ."></p>  
 
         <button class="updateRulesTitle" type="button">تایید</button>
         </div>
         </div>  
-        <ol class="p-3">
-        
+        <ol class="made p-3">
+            <li>
+            <p class="edit" data-placeholder=". . . ."></p>  
+            <button class="updatebtn" type="button">تایید</button>
+            <br>
+          </li>
         </ol>
-        <div class="d-flex justify-content-around position-relative">
-        <p class="w-75 ">
-        <span class="textarea w-100" role="textbox" data-placeholder="نوشتن..." contenteditable></span>
-        </p>
-        <button class="submit" type="button">ثبت</button>
-        <button class="deleterules" type="button">حذف</button>
-        </div>
+        <ol class="tabsare p-3">
+        <li class="position-relative">
+                <span class="hideTabsare">تبصره <span class="numberofTabsare">1</span> :</span>
+                <p class="editTabsare  d-inline-block w-75" data-placeholder="تبصره"></p>  
+                <button class="deleteTabsare disabledbutton" type="button">
+           <svg class='closetbn-svg' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+             <path d="M1.293 1.293a1 1 0 0 1 1.414 0L8 6.586l5.293-5.293a1 1 0 1 1 1.414 1.414L9.414 8l5.293 5.293a1 1 0 0 1-1.414 1.414L8 9.414l-5.293 5.293a1 1 0 0 1-1.414-1.414L6.586 8 1.293 2.707a1 1 0 0 1 0-1.414z"></path>
+           </svg>
+         </button>
+                <button class="addTabsare" type="button">تایید</button>
+                <br>
+              </li>
+        </ol>
         </div>
         `).insertBefore($(".newRules"));
 });
@@ -138,19 +226,17 @@ $(document).on("click", ".updateRulesTitle", function () {
 // حذف ماده
 $(document).on("click", ".deleterules", function () {
   numberofrules--
-  lastRulse--
-  $('.tedadeMade').text(lastRulse)
-  $('.lastRules').text(lastRulse)
+  lastRules--
+  $('.tedadeMade').text(lastRules)
+  $('.lastRules').text(lastRules)
 
-   $(this).parents('div').nextAll(".extraRules").children('.made').children('p').children('.number').each(function(){
+   $(this).parents('div').nextAll(".extraRules").children('.extraMade').children('p').children('.number').each(function(){
    $(this).text(($(this).text())-1)
    
   })
 
   $(this).parents(".extraRules").remove();
 });
-
-
 
 // form Validate/
 (function () {
